964d56c23a8fe623fc51a615771900bccf4d608a
唐明星 <mingxing_tang@163.com>
2014-06-22 22:15:20 +0800

    fix

--- templates/jf_calla-exteriors/html/com_k2/portfolio/item.php
+++ templates/jf_calla-exteriors/html/com_k2/portfolio/item.php
@@ -661,7 +661,7 @@ defined('_JEXEC') or die;
 
 	<?php if(!JRequest::getCmd('print')): ?>
 	<div class="itemBackToTop">
-		<a class="k2Anchor" href="<?php echo $this->item->link; ?>#startOfPageId<?php echo JRequest::getInt('id'); ?>">
+		<a class="k2Anchor" href="/portfolio.html">
 			<?php echo JText::_('K2_BACK_TO_TOP'); ?>
 		</a>
 	</div>
