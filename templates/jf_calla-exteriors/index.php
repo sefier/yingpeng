17d3e35d806bf28e71b46c6fef33886d1c87bee6
Thomson Shine <sjyyt@sjyyt.com>
2014-06-22 22:02:48 +0800

    fix bug

--- templates/jf_calla-exteriors/index.php
+++ templates/jf_calla-exteriors/index.php
@@ -195,5 +195,9 @@ else
 	</div>
 </div>
 <jdoc:include type="modules" name="debug" />
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F12801e0f3c892b349dbfd276cf471ba3' type='text/javascript'%3E%3C/script%3E"));
+</script>
 </body>
 </html>
